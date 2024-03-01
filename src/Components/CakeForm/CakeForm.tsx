789d8bae0fbd6e319312f0ee27944206801c9bb2
Victor Castro <victor.castro34@gmail.com>
2024-03-01 14:11:20 -0300

    feat: updated components to import fetches

--- src/Components/CakeForm/CakeForm.tsx
+++ src/Components/CakeForm/CakeForm.tsx
@@ -1,77 +1,80 @@
-'use client'
-import React, { useState } from 'react';
-import '@/Components/CakeForm/CakeForm.css';
+"use client";
+import React, { useState } from "react";
+import "@/Components/CakeForm/CakeForm.css";
+import { fetchSubmitCakeForm } from "@/api/fetches";
 
 const CakeForm = () => {
-
   const [formData, setFormData] = useState({
-    batterFlavor: '',
-    fillingFlavor: '',
-    icingFlavor: '',
+    cakeName: "",
+    batterFlavor: "",
+    fillingFlavor: "",
+    icingFlavor: "",
   });
 
-  const handleChange = (e: any) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    fetch( '<Link to backend handler>', { // Change this line so it works
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Success:', data)
-      })
-      .catch(error => {
-        console.error('Error:', error)
-      })
-  }
-
+    fetchSubmitCakeForm(formData);
+  };
 
   return (
-    <form className='form-container' onSubmit={handleSubmit}>
-        <h1>Adicione um bolo na database:</h1>
-      <label className='form-label'>
+    <form className="form-container" onSubmit={handleSubmit}>
+      <h1>Adicione um bolo na base de dados:</h1>
+      <label className="form-label">
+        Nome do Bolo:
+        <input
+          required
+          className="form-input"
+          name="cakeName"
+          type="text"
+          onChange={handleChange}
+          value={formData.cakeName}
+        />
+      </label>
+      <label className="form-label">
         Sabor da Massa:
         <input
-          className='form-input'
-          name='batterFlavor'
+          required
+          className="form-input"
+          name="batterFlavor"
           type="text"
           onChange={handleChange}
           value={formData.batterFlavor}
         />
       </label>
-      <label className='form-label'>
+      <label className="form-label">
         Sabor do Recheio:
         <input
-          className='form-input'
-          name='fillingFlavor'
+          required
+          className="form-input"
+          name="fillingFlavor"
           type="text"
           onChange={handleChange}
           value={formData.fillingFlavor}
         />
       </label>
-      <label className='form-label'>
+      <label className="form-label">
         Sabor da Cobertura:
         <input
-          className='form-input'
-          name='icingFlavor'
+          required
+          className="form-input"
+          name="icingFlavor"
           type="text"
           value={formData.icingFlavor}
           onChange={handleChange}
         />
       </label>
-      <div className='button-container'>
-      <button className='form-button' type="submit">Submit</button>
+      <div className="button-container">
+        <button className="form-button" type="submit">
+          Adicionar
+        </button>
       </div>
     </form>
-  )
-}
+  );
+};
 
 export default CakeForm;
--- src/Components/RandomCake/RandomCake.tsx
+++ src/Components/RandomCake/RandomCake.tsx
@@ -1,38 +1,38 @@
-'use client'
-import Cake from '@/Types/Cake';
-import React, { useState } from 'react';
-import '@/Components/RandomCake/RandomCake.css'
+"use client";
+import Cake from "@/Types/Cake";
+import React, { useState } from "react";
+import "@/Components/RandomCake/RandomCake.css";
+import { fetchRandomCake } from "@/api/fetches";
 
 const RandomCake = () => {
   const [randomCake, setRandomCake] = useState<Cake | null>(null);
 
-  const handleSurpriseMe = async () => {
-    try {
-      const response = await fetch('<Link to backend handler>') // Change this line so it works
-      const data = await response.json();
-
-      if (response.ok) {
-        setRandomCake(data);
-      } else {
-        console.error('Error fetching random cake:', data.message);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
-    <div className='random-cake-container'>
-      <h2 className='random-cake-title'>Qual bolo eu faço hoje?</h2>
-      <div className='button-container'>
-      <button className='random-cake-button' onClick={handleSurpriseMe}>Descubra aqui!</button>
+    <div className="random-cake-container">
+      <h2 className="random-cake-title">Qual bolo eu faço hoje?</h2>
+      <div className="button-container">
+        <button
+          className="random-cake-button"
+          onClick={() => fetchRandomCake(setRandomCake)}
+        >
+          Descubra aqui!
+        </button>
       </div>
       {randomCake && (
-        <div className='random-cake-details'>
+        <div className="random-cake-details">
           <h3>Seu bolo aleatório é:</h3>
-          <p>Massa:     <strong>{randomCake.batterFlavor}</strong></p>
-          <p>Recheio:   <strong>{randomCake.fillingFlavor}</strong></p>
-          <p>Cobertura: <strong>{randomCake.icingFlavor}</strong></p>
+          <p>
+            Nome: <strong>{randomCake.cakeName}</strong>
+          </p>
+          <p>
+            Massa: <strong>{randomCake.batterFlavor}</strong>
+          </p>
+          <p>
+            Recheio: <strong>{randomCake.fillingFlavor}</strong>
+          </p>
+          <p>
+            Cobertura: <strong>{randomCake.icingFlavor}</strong>
+          </p>
         </div>
       )}
     </div>
