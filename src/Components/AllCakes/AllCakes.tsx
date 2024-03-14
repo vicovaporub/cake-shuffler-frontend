ab8f8058689b4f1e5fd87d5015221873731142fa
Victor Castro <victor.castro34@gmail.com>
2024-03-14 13:35:48 -0300

    feat: added classes to many html parts

--- src/Components/AllCakes/AllCakes.tsx
+++ src/Components/AllCakes/AllCakes.tsx
@@ -2,25 +2,41 @@
 import Cake from "@/Types/Cake";
 import { fetchAllCakes } from "@/api/fetches";
 import { useState } from "react";
+import "@/Components/AllCakes/AllCakes.css";
 
 export const AllCakes = () => {
   const [cakes, setCakes] = useState<Cake[]>([]);
 
   return (
-    <div>
-      <h1>Lista de bolos:</h1>
-      <button onClick={() => fetchAllCakes(setCakes)}>Ver bolos</button>
+    <div className="all-cakes-container">
+      <h1>Lista de bolos disponíveis:</h1>
+      <button
+        className="all-cakes-button"
+        onClick={() => fetchAllCakes(setCakes)}
+      >
+        Ver bolos
+      </button>
       {cakes.length > 0 && (
-        <ul>
-          {cakes.map((cake) => (
-            <li key={cake._id}>
-              <h2>{cake.cakeName}</h2>
-              <p>Massa: {cake.batterFlavor}</p>
-              <p>Recheio: {cake.fillingFlavor}</p>
-              <p>Cobertura: {cake.icingFlavor}</p>
-            </li>
-          ))}
-        </ul>
+        <div className="all-cakes-list">
+          <ul>
+            {cakes.map((cake) => (
+              <li className="cake-list" key={cake._id}>
+                <h2 className="cake-list-name">{cake.cakeName}</h2>
+                <p className="cake-list-batter">
+                  <strong>Massa: </strong>
+                  {cake.batterFlavor}
+                </p>
+                <p className="cake-list-filling">
+                  <strong>Recheio: </strong>
+                  {cake.fillingFlavor}
+                </p>
+                <p className="cake-list-icing">
+                  <strong>Cobertura: </strong> {cake.icingFlavor}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
       )}
     </div>
   );
